09b2f2ee682cd5a0fc6de39b90f04cf7f6475be5
Jeferson Coutinho <jefissuu@gmail.com>
2023-03-04 15:51:05 -0300

    Used motion layout in detail screen

--- app/src/main/java/com/jefisu/manualplus/features_manual/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/jefisu/manualplus/features_manual/presentation/detail/DetailScreen.kt
@@ -1,47 +1,55 @@
 package com.jefisu.manualplus.features_manual.presentation.detail
 
 import android.net.Uri
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
+import com.jefisu.manualplus.R
+import com.jefisu.manualplus.core.ui.theme.seed
 import com.jefisu.manualplus.core.ui.theme.spacing
 import com.jefisu.manualplus.core.util.ProfileTransitions
 import com.jefisu.manualplus.features_manual.domain.Equipment
+import com.jefisu.manualplus.features_manual.presentation.detail.components.ListSteps
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.skydoves.landscapist.glide.GlideImage
 
+@OptIn(ExperimentalMotionApi::class)
 @Destination(style = ProfileTransitions::class)
 @Composable
 fun DetailScreen(
@@ -49,102 +57,125 @@ fun DetailScreen(
     imageEquipment: Uri?,
     navigator: DestinationsNavigator
 ) {
+    var showSeeMore by remember { mutableStateOf(false) }
+    var animateToEnd by remember { mutableStateOf(false) }
+    val progress by animateFloatAsState(
+        targetValue = if (animateToEnd) 1f else 0f,
+        animationSpec = tween(2000),
+        label = ""
+    )
 
-    Column {
-        Row(
+    val context = LocalContext.current
+    val motionScene = remember {
+        context.resources
+            .openRawResource(R.raw.motion_scene_collapse)
+            .readBytes()
+            .decodeToString()
+    }
+
+    MotionLayout(
+        motionScene = MotionScene(content = motionScene),
+        progress = progress,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        val contentProp by motionProperties("content")
+        val navigationIconProp by motionProperties("navigation_icon")
+        val equipmentNameProp by motionProperties("equipment_name")
+
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(56.dp)
-                .padding(horizontal = MaterialTheme.spacing.small)
+                .layoutId(contentProp.id())
+                .clip(CutCornerShape(topStart = contentProp.distance("corner")))
+                .background(seed)
+        )
+        IconButton(
+            onClick = navigator::popBackStack,
+            modifier = Modifier.layoutId(navigationIconProp.id())
         ) {
-            IconButton(onClick = navigator::popBackStack) {
-                Icon(
-                    imageVector = Icons.Default.ArrowBack,
-                    contentDescription = null
-                )
-            }
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = null,
+                tint = navigationIconProp.color("icon_color")
+            )
         }
-        Column(
-            modifier = Modifier
-                .padding(horizontal = 20.dp)
-                .verticalScroll(rememberScrollState())
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(217.dp)
-            ) {
+        GlideImage(
+            imageModel = { imageEquipment },
+            failure = {
                 Box(
                     contentAlignment = Alignment.Center,
                     modifier = Modifier
-                        .size(150.dp, 217.dp)
-                        .clip(RoundedCornerShape(8.dp))
+                        .matchParentSize()
                         .background(Color(0xFFC9C9C9))
                 ) {
-                    GlideImage(
-                        imageModel = { imageEquipment },
-                        failure = {
-                            Text(
-                                text = "No image",
-                                color = Color.Black,
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 20.sp,
-                                modifier = Modifier.align(Alignment.Center),
-                            )
-                        },
-                        modifier = Modifier
-                            .padding(MaterialTheme.spacing.medium)
-                            .fillMaxWidth()
+                    Text(
+                        text = "No image",
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
                     )
                 }
-                Spacer(modifier = Modifier.width(MaterialTheme.spacing.medium + 4.dp))
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(vertical = MaterialTheme.spacing.extraSmall)
-                ) {
-                    Text(
-                        text = equipment.name,
-                        fontSize = 16.sp
-                    )
-                    Text(
-                        text = "N॰ ${equipment.serialNumber}",
-                        fontSize = 14.sp
-                    )
-                    Text(
-                        text = equipment.description,
-                        color = Color(0xFF888888),
-                        textAlign = TextAlign.Justify,
-                        fontSize = 12.sp,
-                        modifier = Modifier.weight(1f),
-                        lineHeight = 20.sp,
-                        maxLines = 7
-                    )
-                }
-            }
-            Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
-            Text(
-                text = "Setup step by step",
-                fontSize = 16.sp
-            )
-            Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
-            equipment.steps.forEachIndexed { index, step ->
+            },
+            modifier = Modifier
+                .layoutId("image")
+                .clip(RoundedCornerShape(8.dp))
+        )
+        Text(
+            text = equipment.name,
+            fontSize = equipmentNameProp.fontSize("fontSize"),
+            color = equipmentNameProp.color("textColor"),
+            modifier = Modifier.layoutId(equipmentNameProp.id())
+        )
+        Text(
+            text = "N॰ ${equipment.serialNumber}",
+            fontSize = 18.sp,
+            color = seed,
+            modifier = Modifier.layoutId("equipment_serie")
+        )
+        Text(
+            text = equipment.description,
+            color = Color(0xFF888888),
+            textAlign = TextAlign.Justify,
+            fontSize = 12.sp,
+            lineHeight = 18.sp,
+            modifier = Modifier.layoutId("equipment_description"),
+            maxLines = 7
+        )
+        Text(
+            text = "Setup step by step",
+            fontSize = 16.sp,
+            color = Color.White,
+            modifier = Modifier.layoutId("text_setup_aux")
+        )
+
+        ListSteps(
+            steps = equipment.steps,
+            showAllList = animateToEnd,
+            stepsNotDisplayed = { showSeeMore = it || animateToEnd }
+        )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .layoutId("see_more")
+                .padding(horizontal = MaterialTheme.spacing.extraSmall)
+                .clickable(
+                    enabled = showSeeMore,
+                    onClick = { animateToEnd = !animateToEnd }
+                )
+        ) {
+            if (showSeeMore) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = null,
+                    modifier = Modifier.layoutId("arrow_down")
+                )
+                Spacer(modifier = Modifier.width(MaterialTheme.spacing.small))
                 Text(
-                    text = buildAnnotatedString {
-                        append(text = "${index + 1}. ")
-                        withStyle(
-                            style = SpanStyle(color = Color(0xFF888888))
-                        ) {
-                            append(text = step)
-                        }
-                    },
-                    fontSize = 14.sp,
-                    textAlign = TextAlign.Justify,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = MaterialTheme.spacing.medium + 4.dp)
+                    text = if (animateToEnd) "See less" else "See more",
+                    fontSize = 12.sp
                 )
             }
         }
---
+++ app/src/main/java/com/jefisu/manualplus/features_manual/presentation/detail/components/ListSteps.kt
@@ -0,0 +1,87 @@
+package com.jefisu.manualplus.features_manual.presentation.detail.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ListSteps(
+    steps: List<String>,
+    showAllList: Boolean,
+    stepsNotDisplayed: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val heightText = 30.dp
+    val space = 20.dp
+
+    BoxWithConstraints(modifier = modifier) {
+        val numberStepVisible = remember(maxHeight) {
+            if (showAllList) {
+                steps.size
+            } else {
+                (maxHeight / (heightText + space)).toInt()
+            }
+        }
+
+        LaunchedEffect(key1 = numberStepVisible) {
+            stepsNotDisplayed((steps.size - numberStepVisible) > 0)
+        }
+
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(space),
+            userScrollEnabled = showAllList
+        ) {
+            val stepsVisible = steps.take(numberStepVisible)
+            itemsIndexed(stepsVisible) { index, step ->
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(Color.White)
+                        ) {
+                            append(text = "${index + 1}. ")
+                        }
+                        withStyle(
+                            style = SpanStyle(color = Color(0xFF888888))
+                        ) {
+                            append(text = step)
+                        }
+                    },
+                    fontSize = 12.sp,
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .defaultMinSize(minHeight = heightText)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewListSteps() {
+    ListSteps(
+        steps = (1..15).map {
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+                    " Lorem Ipsum has been the industry's standard dummy text"
+        },
+        showAllList = false,
+        stepsNotDisplayed = { n -> }
+    )
+}
